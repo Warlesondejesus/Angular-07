0f476ea4e7af5718312d79feb512b913317664d0
warlesondejesus <warlesondejesus@homail.com>
2022-04-07 21:02:08 -0300

    commit-01

---
+++ src/app/gh-users/components/gh-dialog/gh-dialog.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { GhRepo } from '../../models/ghRepo';
+import { GhUser } from '../../models/ghUser';
+import { GhApiService } from '../../services/gh-api.service';
+
+@Component({
+  selector: 'app-gh-dialog',
+  templateUrl: './gh-dialog.component.html',
+  styleUrls: ['./gh-dialog.component.css']
+})
+export class GhDialogComponent implements OnInit {
+
+  username: string = ''
+  user: GhUser | null = null
+  repo: GhRepo[] | undefined
+
+  constructor(
+    private ghService: GhApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.ghService.findUser(this.username).subscribe(
+      (gUser) => {
+        this.user = gUser
+      }
+    )
+
+    this.ghService.findUserRepositorio(this.username).subscribe(
+      (gRepo) => {
+
+        this.repo= gRepo
+      }
+    )
+  }
+
+}
---
+++ src/app/gh-users/models/ghRepo.ts
@@ -0,0 +1,9 @@
+export interface GhRepo {
+   
+  name:string
+  html_url:string
+  language:string
+  stargazers_count:string
+  public_repos:string
+  description:string
+}
